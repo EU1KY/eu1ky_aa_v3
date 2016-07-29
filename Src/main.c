6beabaaa3dad5b2c2c996620d24e02411f211d28
Yury <kuchura@gmail.com>
2016-07-30 00:59:06 +0300

    Re-enabled data caching

--- Src/main.c
+++ Src/main.c
@@ -223,15 +223,13 @@ static void MPU_Config(void)
   MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
   MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
   MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
   MPU_InitStruct.Number = MPU_REGION_NUMBER1;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
   MPU_InitStruct.SubRegionDisable = 0x00;
   MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
 
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
-
-
 
   /* Enable the MPU */
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
@@ -250,8 +248,8 @@ static void CPU_CACHE_Enable(void)
     SCB_EnableICache();
 
     /* Enable D-Cache */
-    //SCB_InvalidateDCache();
-    //SCB_EnableDCache();
+    SCB_InvalidateDCache();
+    SCB_EnableDCache();
 }
 
 void CRASH(const char *text)
