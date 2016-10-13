3efe6a653c78e4a5e2bb4e06704530052ddc1063
Yury Kuchura <kuchura@gmail.com>
2016-10-13 15:15:09 +0300

    Some cleanup

--- Src/Inc/crash.h
+++ Src/Inc/crash.h
@@ -2,6 +2,15 @@
 #define _CRASH_H
 #include <stdio.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void CRASH(const char* txt);
+
+#ifdef __cplusplus
+}
+#endif
+
 #define CRASHF(fmt, ...) do {char __str[256]; sprintf(__str, fmt, __VA_ARGS__); CRASH(__str); } while ((void)0, 0)
 #endif
