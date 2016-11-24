56913724952dd2cb37a1779534bf53618a7898fd
Yury Kuchura <kuchura@gmail.com>
2016-11-24 14:21:00 +0300

    Changed day and month in timestamp

--- gen_timestamp.bat
+++ gen_timestamp.bat
@@ -1,7 +1,7 @@
 @echo #ifndef BUILD_TIMESTAMP > Src/Inc/build_timestamp.h
 @set hr=%time:~0,2%
 @if "%hr:~0,1%" equ " " set hr=0%hr:~1,1%
-@echo #define BUILD_TIMESTAMP "%date:~-4,4%-%date:~-10,2%-%date:~-7,2% %hr%:%time:~3,2%:%time:~6,2%" >> Src/Inc/build_timestamp.h
+@echo #define BUILD_TIMESTAMP "%date:~-4,4%-%date:~-7,2%-%date:~-10,2% %hr%:%time:~3,2%:%time:~6,2%" >> Src/Inc/build_timestamp.h
 @echo | set /p TMPHG="#define HGREV " >> Src/Inc/build_timestamp.h
 @hg identify -ni >> Src/Inc/build_timestamp.h
 @echo // >> Src/Inc/build_timestamp.h
