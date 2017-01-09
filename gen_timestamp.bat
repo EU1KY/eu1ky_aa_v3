19137fd56825f4829013aaf1cd70205feb6d9fca
EU1KY <kuchura@outlook.com>
2017-01-10 00:32:43 +0300

    Build timestamp creation is now locale-independent

--- gen_timestamp.bat
+++ gen_timestamp.bat
@@ -1,11 +1,23 @@
-@echo #ifndef BUILD_TIMESTAMP > Src/Inc/build_timestamp.h
-@set hr=%time:~0,2%
-@if "%hr:~0,1%" equ " " set hr=0%hr:~1,1%
-@echo #define BUILD_TIMESTAMP "%date:~-4,4%-%date:~-7,2%-%date:~-10,2% %hr%:%time:~3,2%:%time:~6,2%" >> Src/Inc/build_timestamp.h
-@echo | set /p TMPHG="#define HGREV " >> Src/Inc/build_timestamp.h
-@hg identify -ni >> Src/Inc/build_timestamp.h
-@echo // >> Src/Inc/build_timestamp.h
-@echo #define HGREVSTR(s) stringify_(s) >> Src/Inc/build_timestamp.h
-@echo #define stringify_(s) #s >> Src/Inc/build_timestamp.h
-@echo #endif >> Src/Inc/build_timestamp.h
-@echo Src/Inc/build_timestamp.h file created
+@echo off
+echo #ifndef BUILD_TIMESTAMP > Src/Inc/build_timestamp.h
+set hr=%time:~0,2%
+if "%hr:~0,1%" equ " " set hr=0%hr:~1,1%
+
+for /F "skip=1 delims=" %%F in ('
+    wmic PATH Win32_LocalTime GET Day^,Month^,Year /FORMAT:TABLE
+ ') do (
+    for /F "tokens=1-3" %%L in ("%%F") do (
+        set CurrDay=0%%L
+        set CurrMonth=0%%M
+        set CurrYear=%%N ))
+set CurrDay=%CurrDay:~-2%
+set CurrMonth=%CurrMonth:~-2%
+
+echo #define BUILD_TIMESTAMP "%CurrDay%-%CurrMonth%-%CurrYear%%hr%:%time:~3,2%:%time:~6,2%" >> Src/Inc/build_timestamp.h
+echo | set /p TMPHG="#define HGREV " >> Src/Inc/build_timestamp.h
+hg identify -ni >> Src/Inc/build_timestamp.h
+rem echo // >> Src/Inc/build_timestamp.h
+echo #define HGREVSTR(s) stringify_(s) >> Src/Inc/build_timestamp.h
+echo #define stringify_(s) #s >> Src/Inc/build_timestamp.h
+echo #endif >> Src/Inc/build_timestamp.h
+echo Src/Inc/build_timestamp.h file created
