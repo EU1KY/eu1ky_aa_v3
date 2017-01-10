1f05e57d7968d4eec48de586cce8e8611219a9de
Yury <kuchura@gmail.com>
2017-01-10 21:04:38 +0300

    Warning if no repo in sources

--- gen_timestamp.bat
+++ gen_timestamp.bat
@@ -1,23 +1,35 @@
 @echo off
 echo #ifndef BUILD_TIMESTAMP > Src/Inc/build_timestamp.h
-set hr=%time:~0,2%
-if "%hr:~0,1%" equ " " set hr=0%hr:~1,1%
 
-for /F "skip=1 delims=" %%F in ('
-    wmic PATH Win32_LocalTime GET Day^,Month^,Year /FORMAT:TABLE
- ') do (
-    for /F "tokens=1-3" %%L in ("%%F") do (
-        set CurrDay=0%%L
-        set CurrMonth=0%%M
-        set CurrYear=%%N ))
-set CurrDay=%CurrDay:~-2%
-set CurrMonth=%CurrMonth:~-2%
-
-echo #define BUILD_TIMESTAMP "%CurrDay%-%CurrMonth%-%CurrYear%%hr%:%time:~3,2%:%time:~6,2%" >> Src/Inc/build_timestamp.h
+hg stat > NUL
+if ERRORLEVEL 1 goto HGERR
 echo | set /p TMPHG="#define HGREV " >> Src/Inc/build_timestamp.h
 hg identify -ni >> Src/Inc/build_timestamp.h
-rem echo // >> Src/Inc/build_timestamp.h
+
+:MAKE_TIMESTAMP
+for /F "skip=1 delims=" %%F in ('
+    wmic PATH win32_utctime GET Day^,Month^,Hour^,Minute^,Year /FORMAT:TABLE
+ ') do (
+    for /F "tokens=1-5" %%L in ("%%F") do (
+        set CurrDay=0%%L
+        set CurrHour=0%%M
+        set CurrMinute=0%%N
+        set CurrMonth=0%%O
+        set CurrYear=%%P))
+set CurrDay=%CurrDay:~-2%
+set CurrMonth=%CurrMonth:~-2%
+set CurrHour=%CurrHour:~-2%
+set CurrMinute=%CurrMinute:~-2%
+
+echo #define BUILD_TIMESTAMP "%CurrYear%-%CurrMonth%-%CurrDay% %CurrHour%:%CurrMinute% UT">> Src/Inc/build_timestamp.h
 echo #define HGREVSTR(s) stringify_(s) >> Src/Inc/build_timestamp.h
 echo #define stringify_(s) #s >> Src/Inc/build_timestamp.h
 echo #endif >> Src/Inc/build_timestamp.h
-echo Src/Inc/build_timestamp.h file created
+echo Src/Inc/build_timestamp.h file created at %CurrYear%-%CurrMonth%-%CurrDay% %CurrHour%:%CurrMinute% UT
+:END
+exit 0
+
+:HGERR
+echo #warning Mercurial failed. Repository not found. Firmware revision will not be generated. >> Src/Inc/build_timestamp.h
+echo #define HGREV "N/A" >> Src/Inc/build_timestamp.h
+goto MAKE_TIMESTAMP
